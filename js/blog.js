700692a4ed81d7c18ee24a7ef2cc492222aaf277
Darren <dmascioli1223@gmail.com>
2019-12-19 21:48:38 -0500

    testing blog script

---
+++ js/blog.js
@@ -0,0 +1,32 @@
+
+
+
+function load_blog() {
+    convert_md()
+}
+
+function convert_md() {
+    var text
+
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'posts/nametags.md');
+    httpRequest.send();
+
+    httpRequest.onreadystatechange = () => {
+        if(httpRequest.readyState === XMLHttpRequest.DONE) {
+            if(httpRequest.status === 200) {
+                // if successful, show the response
+                text = httpRequest.responseText
+            } else {
+                // otherwise, oh no...
+                alert('There was a problem with the request.')
+            }
+        }
+    }
+
+    target = document.getElementById('test1'),
+        converter = new showdown.Converter(),
+        html = converter.makeHtml(text);
+    
+    target.innerHTML = html;
+}
---
+++ posts/nametags.md
@@ -0,0 +1,3 @@
+# How to create name tags in Inkscape
+
+As conference director for [NAESC](https://naesc.org)'s Engineering Leadership Summit, it was required to create name tags for over 300 attendees. Each name tag had to include everyone's name, major, school, and other conference information that told people where to go. Individually editing these seemed like torture, so I went looking for a solution. I came across the Generator plugin for Inkscape, and after some fiddingling, it turned out to be exactly what I needed. I decided to write down the process for doing this because I didn't find a whole lot of documentation, so it took a lot of guessing and checking to get the plugin to work.
